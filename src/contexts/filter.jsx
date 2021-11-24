e8d06cf8e0dadfd0e87dc7d9dee5dccf7943cceb
Luca Rodrigues <luca12rodrigues@gmail.com>
2021-11-23 22:37:02 -0300

    fix: fix merging

--- src/contexts/filter.jsx
+++ src/contexts/filter.jsx
@@ -2,11 +2,7 @@ import { createContext, useState, useEffect } from "react";
 
 import { api } from '../services/api'
 
-<<<<<<< HEAD
-import { formatEmployees, filterEmployees } from '../utils'
-=======
 import { formatEmployees, filterEmployees, sortEmployees } from '../utils'
->>>>>>> feature-column-sorting
 
 export const FilterContext = createContext({})
 
@@ -18,8 +14,6 @@ export const FilterProvider = (props) => {
   function getFilteredEmployees(filterString) {
     const filteredEmployees = filterEmployees(filterString, totalEmployees)
     setEmployees(filteredEmployees)
-<<<<<<< HEAD
-=======
   }
 
   function getSortedEmployees(sortedField, sortDirection) {
@@ -27,7 +21,6 @@ export const FilterProvider = (props) => {
     const sortedTotalEmployees = sortEmployees(sortedField, totalEmployees, sortDirection)
     setEmployees(sortedEmployees)
     setTotalEmployees(sortedTotalEmployees)
->>>>>>> feature-column-sorting
   }
 
   useEffect(() => {
@@ -46,11 +39,7 @@ export const FilterProvider = (props) => {
   }, [])
 
   return (
-<<<<<<< HEAD
-    <FilterContext.Provider value={{ employees, getFilteredEmployees, isLoading }}>
-=======
     <FilterContext.Provider value={{ employees, getFilteredEmployees, getSortedEmployees, isLoading }}>
->>>>>>> feature-column-sorting
       {props.children}
     </FilterContext.Provider>
   )
